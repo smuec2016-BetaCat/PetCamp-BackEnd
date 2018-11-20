221de606b5340ac9590bd5fa939a8785dba2d324
Azzinothz <youyihao@gmail.com>
2018-11-20 16:23:05 +0800

    feat: create data tables when starting if not exist

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://{}:{}@{}:{}/{}".format(
     DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
 
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 api.add_resource(CounterAPI, "/api/v0/counter")
 
