49f987fc436a0edad30aa265fd09d9ca24db3a2e
Azzinothz <30699803+Azzinothz@users.noreply.github.com>
2018-11-26 02:01:15 +0800

    feat: add register api doc: add some new docs

---
+++ resources/authAPI.py
@@ -0,0 +1,27 @@
+from flask import request
+from flask_restful import Resource
+from models.user import User, db
+import datetime
+
+acao = {"Access-Control-Allow-Origin": "*"}
+
+
+class AuthAPI(Resource):
+    """
+    Authorization API
+    """
+    @staticmethod
+    def post():
+        """
+        Register
+        :return: success or error message
+        """
+        username = request.json.get("username")
+        password = request.json.get("password")
+        if User.query.filter_by(username=username).first() is not None:
+            return {"error": "User name has already been taken."}, 400, acao
+        user = User(username=username, create_time=datetime.datetime.now())
+        user.hash_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return {"msg": "Success"}, 201, acao
--- resources/counterAPI.py
+++ resources/counterAPI.py
@@ -2,24 +2,41 @@ from flask_restful import Resource
 from models.counter import Counter, db
 import datetime
 
+acao = {"Access-Control-Allow-Origin": "*"}
+
 
 class CounterAPI(Resource):
+    """
+    Counter API
+    """
     @staticmethod
     def get():
-        return {"count": len(Counter.query.all())}, 200, {"Access-Control-Allow-Origin": "*"}
+        """
+        Get the number of records
+        :return: the number of records
+        """
+        return {"count": len(Counter.query.all())}, 200, acao
 
     @staticmethod
     def post():
+        """
+        Insert a new record
+        :return: insert time
+        """
         current_datetime = datetime.datetime.now()
         visit = Counter(date_time=current_datetime)
         db.session.add(visit)
         db.session.commit()
         return {
                    "insert_time": current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-               }, 200, {"Access-Control-Allow-Origin": "*"}
+               }, 200, acao
 
     @staticmethod
     def delete():
+        """
+        Clear all records in the datatable
+        :return: The number of deleted rows
+        """
         deleted_rows = Counter.query.delete()
         db.session.commit()
-        return {"deleted_rows": deleted_rows}, 200, {"Access-Control-Allow-Origin": "*"}
+        return {"deleted_rows": deleted_rows}, 200, acao
